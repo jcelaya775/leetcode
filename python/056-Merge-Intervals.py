2e89e20033e2650d0056cc134522889b8d542fc4
jcelaya775 <jcelaya775@gmail.com>
2022-10-31 09:01:48 -0500

    new problems

---
+++ python/056-Merge-Intervals.py
@@ -0,0 +1,24 @@
+from typing import List
+
+# https://leetcode.com/problems/merge-intervals/https://leetcode.com/problems/merge-intervals/
+
+class Solution:
+    def mergeIntervals(self, intervals: List[List[int]]) -> List[List[int]]:
+        res = [intervals[0]]
+        intervals.sort(key=lambda interval: interval[0])
+        for start, end in intervals[1:]:
+            if start <= res[-1][1]:
+                res[-1][1] = max(end, res[-1][1])
+            else:
+                res.append([start, end])
+        return res
+
+
+
+def main():
+    s = Solution()
+    intervals = [[1, 3], [5, 6], [2, 5], [10, 12]]
+    print(s.mergeIntervals(intervals))
+
+
+main()
---
+++ python/1448-Count-Good-Nodes-in-Binary-Tree.py
@@ -0,0 +1,42 @@
+# https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+class Solution:
+    class TreeNode:
+        def __init__(self, val, left=None, right=None):
+            self.val = val
+            self.left = left
+            self.right = right
+
+    def goodNodes(self, root):
+        def dfs(node, maxVal):
+            if not node:
+                return 0
+
+            res = 1 if node.val >= maxVal else 0
+            maxVal = max(node.val, maxVal)
+            res += dfs(node.left, maxVal)        
+            res += dfs(node.right, maxVal)
+            return res
+
+        return dfs(root, float("-inf"))
+
+
+def main():
+    s = Solution()
+    tree = s.TreeNode(2)
+    three = s.TreeNode(3)
+    four = s.TreeNode(4)
+    five = s.TreeNode(5)
+    one = s.TreeNode(1)
+    seven = s.TreeNode(7)
+
+    tree.left = three
+    tree.right = four
+    three.left = five
+    three.right = one
+    four.right = seven
+
+    print(s.goodNodes(tree))
+
+
+main()
